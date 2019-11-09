fdd0880f9c5a1990815237a2964ce671539ccf71
kazuma1989 <funifuni.1204@gmail.com>
2019-11-09 12:28:43 +0900

    feat: Add full examples

--- reinventing-redux/index.tsx
+++ reinventing-redux/index.tsx
@@ -1,13 +1,230 @@
-import React from 'react'
+import React, { useState, useReducer } from 'react'
 import ReactDOM from 'react-dom'
 
-function App() {
-  return <h1>Hello</h1>
+// --------------------
+// State
+
+type State = {
+  count: number
 }
+
+// --------------------
+// useState
+
+function AppUseState() {
+  const [state, setState] = useState<State>({ count: 0 })
+
+  const increment = () => {
+    setState(prevState => ({
+      ...prevState,
+      count: prevState.count + 1,
+    }))
+  }
+  const decrement = () => {
+    setState(prevState => ({
+      ...prevState,
+      count: prevState.count - 1,
+    }))
+  }
+
+  return (
+    <div>
+      {state.count}
+      <button type="button" onClick={increment}>
+        +
+      </button>
+      <button type="button" onClick={decrement}>
+        -
+      </button>
+    </div>
+  )
+}
+
+// --------------------
+// wrap useState
+
+function useMyStateHook() {
+  const [state, setState] = useState<State>({ count: 0 })
+
+  const increment = () => {
+    setState(prevState => ({
+      ...prevState,
+      count: prevState.count + 1,
+    }))
+  }
+  const decrement = () => {
+    setState(prevState => ({
+      ...prevState,
+      count: prevState.count - 1,
+    }))
+  }
+
+  return {
+    state,
+    increment,
+    decrement,
+  }
+}
+
+function AppUseStateWithMyHooks() {
+  const { state, increment, decrement } = useMyStateHook()
+
+  return (
+    <div>
+      {state.count}
+      <button type="button" onClick={increment}>
+        +
+      </button>
+      <button type="button" onClick={decrement}>
+        -
+      </button>
+    </div>
+  )
+}
+
+// --------------------
+// useState and reducer
+
+type Action =
+  | {
+      type: 'INCREMENT'
+    }
+  | {
+      type: 'DECREMENT'
+    }
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {
+        ...state,
+        count: state.count + 1,
+      }
+    }
+
+    case 'DECREMENT': {
+      return {
+        ...state,
+        count: state.count - 1,
+      }
+    }
+  }
+}
+
+function AppUseStateWithReducer() {
+  const [state, setState] = useState<State>({ count: 0 })
+  const dispatch = (action: Action) =>
+    setState(prevState => reducer(prevState, action))
+
+  const increment = () => {
+    dispatch({
+      type: 'INCREMENT',
+    })
+  }
+  const decrement = () => {
+    dispatch({
+      type: 'DECREMENT',
+    })
+  }
+
+  return (
+    <div>
+      {state.count}
+      <button type="button" onClick={increment}>
+        +
+      </button>
+      <button type="button" onClick={decrement}>
+        -
+      </button>
+    </div>
+  )
+}
+
+// --------------------
+// wrap useState and reducer
+
+function useMyReducer(
+  reducer: (state: State, action: Action) => State,
+  initialState: State,
+) {
+  const [state, setState] = useState(initialState)
+  const dispatch = (action: Action) =>
+    setState(prevState => reducer(prevState, action))
+
+  return [state, dispatch] as const
+}
+
+function AppUseMyReducer() {
+  const [state, dispatch] = useMyReducer(reducer, { count: 0 })
+
+  const increment = () => {
+    dispatch({
+      type: 'INCREMENT',
+    })
+  }
+  const decrement = () => {
+    dispatch({
+      type: 'DECREMENT',
+    })
+  }
+
+  return (
+    <div>
+      {state.count}
+      <button type="button" onClick={increment}>
+        +
+      </button>
+      <button type="button" onClick={decrement}>
+        -
+      </button>
+    </div>
+  )
+}
+
+// --------------------
+// useReducer
+
+function AppUseReducer() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
+
+  const increment = () => {
+    dispatch({
+      type: 'INCREMENT',
+    })
+  }
+  const decrement = () => {
+    dispatch({
+      type: 'DECREMENT',
+    })
+  }
+
+  return (
+    <div>
+      {state.count}
+      <button type="button" onClick={increment}>
+        +
+      </button>
+      <button type="button" onClick={decrement}>
+        -
+      </button>
+    </div>
+  )
+}
+
+// --------------------
+// bootstrap
 
 ReactDOM.render(
   <div>
-    <App />
+    <AppUseState />
+    <hr />
+    <AppUseStateWithMyHooks />
+    <hr />
+    <AppUseStateWithReducer />
+    <hr />
+    <AppUseMyReducer />
+    <hr />
+    <AppUseReducer />
   </div>,
   document.getElementById('root'),
 )
