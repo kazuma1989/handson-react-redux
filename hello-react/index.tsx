afbe55b0ea73fc845964e09a6260744a23187d21
kazuma1989 <funifuni.1204@gmail.com>
2019-11-09 10:02:43 +0900

    feat: Implement example variations

--- hello-react/index.tsx
+++ hello-react/index.tsx
@@ -1,7 +1,37 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-function App() {
+function AppStatic() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
+
+function AppStaticState() {
+  const [name, setName] = useState('')
+
+  return (
+    <div>
+      <h1>Hello {name}!</h1>
+      <input value={name} />
+    </div>
+  )
+}
+
+function AppInputSimple() {
+  const [name, setName] = useState('')
+
+  return (
+    <div>
+      <h1>Hello {name}!</h1>
+      <input value={name} onChange={e => setName(e.target.value)} />
+    </div>
+  )
+}
+
+function AppInput() {
   const [name, setName] = useState('')
 
   return (
@@ -12,4 +42,89 @@ function App() {
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+function AppCheckbox() {
+  const [checked, setChecked] = useState(false)
+  const [name, setName] = useState('')
+
+  return (
+    <div>
+      <h1>Hello {name.toUpperCase() || 'world'}!</h1>
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={e => setChecked(e.target.checked)}
+      />
+
+      {checked && (
+        <input value={name} onChange={e => setName(e.target.value)} />
+      )}
+    </div>
+  )
+}
+
+function AppInputArrayStatic() {
+  const [nameList, setNameList] = useState([''])
+  const add = () => setNameList(list => [...list, ''])
+
+  return (
+    <div>
+      <h1>Hello {nameList.join(' and ')}!</h1>
+      <button type="button" onClick={add}>
+        Add
+      </button>
+
+      {nameList.map(name => (
+        <input value={name} />
+      ))}
+    </div>
+  )
+}
+
+function AppInputArray() {
+  const [nameList, setNameList] = useState([''])
+  const add = () => setNameList(list => [...list, ''])
+
+  return (
+    <div>
+      <h1>Hello {nameList.join(' and ')}!</h1>
+      <button type="button" onClick={add}>
+        Add
+      </button>
+
+      {nameList.map((name, i) => (
+        <input
+          value={name}
+          onChange={e => {
+            const newName = e.target.value
+
+            setNameList(list => {
+              const newNameList = [...list]
+              newNameList[i] = newName
+
+              return newNameList
+            })
+          }}
+        />
+      ))}
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <div>
+    <AppStatic />
+    <hr />
+    <AppStaticState />
+    <hr />
+    <AppInputSimple />
+    <hr />
+    <AppInput />
+    <hr />
+    <AppCheckbox />
+    <hr />
+    <AppInputArrayStatic />
+    <hr />
+    <AppInputArray />
+  </div>,
+  document.getElementById('root'),
+)
