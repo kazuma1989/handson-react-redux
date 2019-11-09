a4114e5efe4ad75eed231e7fe6d8eee9e2340263
kazuma1989 <funifuni.1204@gmail.com>
2019-11-09 11:20:24 +0900

    feat: Add another example skeleton

---
+++ reinventing-redux/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return <h1>Hello</h1>
+}
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById('root'),
+)
