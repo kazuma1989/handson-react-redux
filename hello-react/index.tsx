cbda10c3a422f2b3d6a695427347af1d131ad91a
kazuma1989 <funifuni.1204@gmail.com>
2019-11-09 09:29:27 +0900

    feat: Add an interactive example

--- hello-react/index.tsx
+++ hello-react/index.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 function App() {
-  return <div>Hello</div>
+  const [name, setName] = useState('')
+
+  return (
+    <div>
+      <h1>Hello {name.toUpperCase() || 'world'}!</h1>
+      <input value={name} onChange={e => setName(e.target.value)} />
+    </div>
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('root'))
